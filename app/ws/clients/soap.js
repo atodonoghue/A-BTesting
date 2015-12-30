1cff9520680f7b9072470bc764343c8937711af9
Austin O'Donoghue <austin.odonoghue@gmail.com>
2015-12-30 14:41:12 -0500

    Updated.

--- app/ws/clients/soap.js
+++ app/ws/clients/soap.js
@@ -1,49 +1,33 @@
-var soap = require('soap');
-var parseString = require('xml2js').parseString;
-var js2xmlparser = require('js2xmlparser');
 var fs = require('fs');
 var pd = require('pretty-data').pd;
+var http = require('http');
 
 fs.readFile('./app/resources/request.xml', 'utf8', function (err, data) {
     if (err) throw err;
     console.log('******* XML Input *******');
     console.log(data);
-    parseString(data, {attrkey: '', mergeAttrs: true }, function (err, parseResult) {
-        var request = parseResult;
-
-        request['soapenv:Envelope']['attributes'] = new Array();
-        for (var attributes in request['soapenv:Envelope']) {
-            if (attributes.substring(0, 5) == 'xmlns') {
-                request['soapenv:Envelope']['attributes'][attributes] = new Array();
-                request['soapenv:Envelope']['attributes'][attributes] = request['soapenv:Envelope'][attributes][0];
-                delete request['soapenv:Envelope'][attributes];
+        var post_options = {
+            host: 'yywasr.testroyal.tstsh.tstrccl.com',
+            port: '13040',
+            path: '/efcweb/services/ws/v1/EFCUtilService',
+            method: 'POST',
+            headers: {
+                'Content-Type': 'text/xml;charset=UTF-8',
+                'Content-Length': Buffer.byteLength(data)
             }
-            else {
-                for (var keyPair in console.log(request['soapenv:Envelope']['attributes']))
-                    console.log(keyPair);
-                break;
-            }
-        }
+        };
+        var post_req = http.request(post_options, function(response) {
+            response.setEncoding('utf8');
+            var str;
+            response.on('data', function (chunk) {
+                str += chunk;
+            });
 
-        console.log('******* JSON Request *******');
-        console.log(pd.json(request));
-
-        var methodName = Object.keys(request['soapenv:Envelope']['soapenv:Body'][0])[0];
-        var i = methodName.search(':');
-        methodName = methodName.substring(i + 1, methodName.length - i + 3);
-        console.log('******* Method Name *******');
-        console.log(methodName);
-
-
-        var wsdl = 'http://efcdev01:16120/efcweb/services/ws/v1/VoyageService?wsdl';
-        soap.createClient(wsdl, function(err, client) {
-
-            client[methodName](request, function(err, result, body) {
-                console.log('******* XML Request *******');
-                console.log(js2xmlparser('ignore-me', request));
+            response.on('end', function () {
                 console.log('******* XML Response *******');
-                console.log(pd.xml(body));
+                console.log(pd.xml(str));
             });
         });
-    });
+        post_req.write(data);
+        post_req.end();
 });
