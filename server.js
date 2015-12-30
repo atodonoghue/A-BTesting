857919ca2d8e8e9752f08c9883b078329e4f2170
Austin O'Donoghue <austin.odonoghue@gmail.com>
2015-12-29 20:19:05 -0500

    Created repo.

--- server.js
+++ server.js
@@ -5,7 +5,6 @@
 
 /** Server Setup **/
 var express = require('express');
-var mongoose   = require('mongoose');
 var bodyParser = require('body-parser');
 var serverRouter = require('./app/rs/servers/router.js');
 
@@ -14,10 +13,6 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 /** End Server Setup **/
 
-/** Database Setup **/
-//mongoose.connect('mongodb://node:node@novus.modulusmongo.net:27017/Iganiq8o');
-/** End Database Setup **/
-
 /** Application Initialization **/
 app.use('/api', serverRouter.router);
 var port = process.env.PORT || 3000;
@@ -25,5 +20,7 @@ app.listen(port);
 
 console.log('Magic happens on port ' + port);
 
-var soapClient = require('./app/ws/clients/soap.js');
+/** SOAP Client Test **/
+require('./app/ws/clients/soap.js');
+
 /** Why are you looking here? This party never stops... **/
